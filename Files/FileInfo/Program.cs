5b5492f81434fe5b404edcfebbb7e397c36d4708
Chidera <deranoel25@yahoo.com>
2025-07-09 21:13:16 +0100

    overview fileInfo

--- Files/FileInfo/Program.cs
+++ Files/FileInfo/Program.cs
@@ -11,10 +11,31 @@ if (!File.Exists(filename)) {
 }
 
 // TODO: Get some information about the file
-
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
 
 // TODO: We can also get general information using a FileInfo 
-
+FileInfo fileInfo = new FileInfo(filename);
+Console.WriteLine($"File Name: {fileInfo.Name}");
+Console.WriteLine($"File Size: {fileInfo.Length} bytes");
+Console.WriteLine($"File Creation Time: {fileInfo.CreationTime}");
 
 // TODO: File information can also be manipulated
+if (fileInfo.Exists) {
+    // Rename the file
+    string newFileName = "RenamedFile.txt";
+    fileInfo.MoveTo(newFileName);
+    Console.WriteLine($"File renamed to: {newFileName}");
+
+    // Get the new file info
+    FileInfo newFileInfo = new FileInfo(newFileName);
+    Console.WriteLine($"New File Name: {newFileInfo.Name}");
+    Console.WriteLine($"New File Size: {newFileInfo.Length} bytes");
+    Console.WriteLine($"New File Creation Time: {newFileInfo.CreationTime}");
+    
+} else {
+    Console.WriteLine("The file does not exist.");
+}
+
 
